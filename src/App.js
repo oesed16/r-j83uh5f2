a555513731b177b08649fc06e70fb5a4ad0810cb
Angela Castagno <oesed16@gmail.com>
2018-12-19 23:49:50 -0500

    Reto Contador de caracteres React

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      length: 0
+    };
+  }
+  charCount = event => {
+    this.setState({
+      length: event.target.value.length
+    });
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.charCount}></textarea>
+        <div className="counter">{this.state.length}</div>
       </div>
-    );
+    )
   }
 }
 
